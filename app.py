dd681ad34fb2ee3017e545bf3b81c1a9369a85c0
Winnie <zxc898977@gmail.com>
2021-02-16 11:32:14 +0800

    ok

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import time
 #======python的函數庫==========
 
 
-"""
+
 
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
@@ -30,18 +30,6 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 line_bot_api = LineBotApi('eWXxSdmlYhI3wMqMDs1srmeGW2a+/bGMKP/7jFRnEproALIDF+TVoKxUT3YGLEBwG19lhJK93GSH9K47f1bQsG0s4kY5ioe6le0O5xRQ9K1nqzqhUt4xTMUlg7Y8Yqc5tqasvrlilZiRykTBabf98wdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('2888aa27a4a024de8e1ceb9fb4e3ef3d')
-"""
-
-import configparser
-
-# LINE 聊天機器人的基本資料
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
-
-
 
 
 # 監聽所有來自 /callback 的 Post Request
@@ -64,27 +52,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    if '粉絲團' or "FB" in msg:
+    if '粉絲團' or "FB" or "活動" in msg:
         message = imagemap_message()
         line_bot_api.reply_message(event.reply_token, message)
-
-    """
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-       """ 
 
     else:#如果非以上的選項，就會學你說話
         message = TextSendMessage(text=msg)
