1ce55b01cc98a579f120735cb3660eca415842a2
Winnie <zxc898977@gmail.com>
2021-02-16 11:36:25 +0800

    ok

--- read_config.py
+++
@@ -1,7 +0,0 @@
-import configparser
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-for i in config.sections():
-    for j in config[i]:
-        print(f'{config[i]}\t: {j}\t: {config.get(i, j)}')
